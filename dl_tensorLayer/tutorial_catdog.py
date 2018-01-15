fa01934df139a6438f6c33b8371d1b4df620fb86
jarvisqi <qdw8911@sina.com>
2018-01-15 18:11:21 +0800

    update params

--- dl_tensorLayer/tutorial_catdog.py
+++ dl_tensorLayer/tutorial_catdog.py
@@ -1,8 +1,8 @@
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
-import numpy as np 
-import tensorflow as tf 
-import tensorlayer as tl 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import numpy as np
+import tensorflow as tf
+import tensorlayer as tl
 from skimage import io, transform
 from keras.preprocessing import image as kimg
 from keras.utils import np_utils
@@ -27,7 +27,7 @@ def load_data():
     images = []
     labels = []
     for f in files:
-        img = kimg.load_img(path + f, target_size=(img_h,img_w))
+        img = kimg.load_img(path + f, target_size=(img_h, img_w))
         img_array = kimg.img_to_array(img)
         images.append(img_array)
         if 'cat' in f:
@@ -50,29 +50,32 @@ def build_net(x):
     """
     print("build network")
     network = tl.layers.InputLayer(x, name='input_layer')
-    network = tl.layers.Conv2d(network, n_filter=64, filter_size=(3, 3), act=tf.nn.relu, name='conv2d1')
+    network = tl.layers.Conv2d(network, n_filter=64, filter_size=(5, 5), act=tf.nn.relu, name='conv2d1')
     network = tl.layers.MaxPool2d(network, name='maxpool1')
     network = tl.layers.DropoutLayer(network, keep=0.5, name='dropout1')
 
-    network = tl.layers.Conv2d(network, n_filter=128, filter_size=(3, 3), act=tf.nn.relu, name='conv2d2')
+    network = tl.layers.Conv2d(network, n_filter=128, filter_size=(5, 5), act=tf.nn.relu, name='conv2d2')
     network = tl.layers.MaxPool2d(network, name='maxpool2')
     network = tl.layers.DropoutLayer(network, keep=0.5, name='dropout2')
 
-    network = tl.layers.Conv2d(network, n_filter=256, filter_size=(3, 3), act=tf.nn.relu, name='conv2d3')
+    network = tl.layers.Conv2d(network, n_filter=256, filter_size=(5, 5), act=tf.nn.relu, name='conv2d3')
     network = tl.layers.MaxPool2d(network, name='maxpool3')
     network = tl.layers.DropoutLayer(network, keep=0.5, name='dropout3')
+
+    network = tl.layers.Conv2d(network, n_filter=512, filter_size=(5, 5), act=tf.nn.relu, name='conv2d4')
+    network = tl.layers.MaxPool2d(network, name='maxpool4')
+    network = tl.layers.DropoutLayer(network, keep=0.5, name='dropout4')
 
     network = tl.layers.FlattenLayer(network, name='flatten_layer')
     network = tl.layers.DenseLayer(network, n_units=512, act=tf.nn.relu, name='dense_layer1')
     network = tl.layers.DropoutLayer(network, keep=0.5, name='dropout5')
-    
+
     network = tl.layers.DenseLayer(network, n_units=n_classes, act=tf.identity, name='output')
-    
+
     return network
 
 
 def cost_fn(network):
-
     # 定义损失函数
     y = network.outputs
     # 交叉熵损失函数
@@ -84,6 +87,9 @@ def cost_fn(network):
 
 
 def train():
+    """
+    训练模型
+    """
     sess = tf.InteractiveSession()
 
     print("load data......")
@@ -92,7 +98,7 @@ def train():
     lables = np.load("./data/cd_label.npy")
     images /= 255
     X_train, X_test, y_train, y_test = train_test_split(images, lables, test_size=0.2)
-    print(X_train.shape,y_train.shape,X_test.shape,y_test.shape)
+    print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
     network = build_net(x)
     cost, acc = cost_fn(network)
@@ -100,6 +106,9 @@ def train():
     # 定义优化器
     train_params = network.all_params
     train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost, var_list=train_params)
+
+    tf.summary.scalar('acc', acc)
+    # tf.summary.scalar('cost', cost)
     # 初始化 Session 中所有参数
     tl.layers.initialize_global_variables(sess)
     # 打印模型和参数信息
@@ -109,17 +118,18 @@ def train():
     print("fit......")
     # 训练
     tl.utils.fit(sess, network, train_op, cost, X_train, y_train, x, y_,
-                 acc=acc, batch_size=256, n_epoch=64, print_freq=1, X_val=X_test, y_val=y_test, eval_train=True)
+                 acc=acc, batch_size=256, n_epoch=64, print_freq=2, X_val=X_test, y_val=y_test,
+                 tensorboard=True)
     print("test......")
     # 评估模型
-    tl.utils.test(sess, network, acc, X_test, y_test)
+    tl.utils.test(sess, network, acc, X_test, y_test, x, y_, batch_size=256)
     tl.files.save_npz(network.all_params, name='./models/tl_catdog_model.npz')
 
     sess.close()
 
 
 def test_predict():
-    
+
     sess = tf.InteractiveSession()
     network = build_net(x)
 
@@ -137,10 +147,9 @@ def test_predict():
     sess.close()
 
 
-
 if __name__ == '__main__':
-    
-    print(x.shape,y_.shape)
+
+    print(x.shape, y_.shape)
 
     # load_data()
 
