b049f8684794ad917b0d903510957e294484717c
jarvisqi <qdw8911@sina.com>
2018-02-07 18:44:39 +0800

    PyTorch adjust parameter

--- dl_torch/torch_CIFAR10.py
+++ dl_torch/torch_CIFAR10.py
@@ -1,83 +1,68 @@
 # -*- coding: utf-8 -*-
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import numpy as np 
 import torch
 import torch.nn.functional as fn
 from torch.autograd import Variable
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-def load_data():
+def load_data(batch_size):
     """
     加载数据
     """
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     trainset = torchvision.datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=4)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=4)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=4)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=4)
 
     return train_loader, test_loader
 
 
-def train(epochs):
+def train(epochs=None, batch_size=32,log_interval=None):
 
-    train_loader, test_loader = load_data()
+    train_loader, test_loader = load_data(batch_size)
 
     net = Net()
-
+    net.cuda()
     # 定义损失函数和优化器
     loss_fun = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
-    all_losses = []
     for epoch in range(epochs):
-        running_loss = 0.0
-        for step, (X_train, y_train) in enumerate(train_loader, 0):
-            X_train, y_train = Variable(X_train), Variable(y_train)
 
-            optimizer.zero_grad()                   # 清空上一步的残余更新参数值
-            outs = net(X_train)                     # 输入训练数据 x, 输出分析值
-            loss = loss_fun(outs, y_train)          # 计算两者的误差
+        epoch_loss, epoch_acc = 0, 0
+        for step, (X_train, y_train) in enumerate(train_loader, 0):
+            acc_train = y_train.cuda()
+            X_train, y_train = Variable(X_train).cuda(), Variable(y_train).cuda()
+
+            optimizer.zero_grad()                   # 每次迭代清空上一次的梯度
+            outputs = net(X_train)                  # 输入训练数据 x, 输出分析值
+            loss = loss_fun(outputs, y_train)       # 计算两者的误差
             loss.backward()                         # 误差反向传播, 计算参数更新值
             optimizer.step()                        # 将参数更新值施加到 net 的 parameters 上
+            
+            predicted = torch.max(outputs, 1)[1].cuda().data.squeeze()
+            epoch_acc += torch.sum(predicted == acc_train) / y_train.size(0)
+            epoch_loss += loss.data[0]
+            if step % log_interval == 0:
+                print('Epoch: {} \ttrain loss: {:.4f} \taccuracy: {:.4f}'.format(epoch+1, epoch_loss/log_interval, epoch_acc/log_interval))
+                epoch_loss, epoch_acc = 0, 0
 
-            running_loss += loss.data[0]
-            if i % 2000 == 1999:
-                print('[{0}, {1}] loss: {2:.4f}'.format(epoch + 1, i + 1, running_loss / 2000))
-                all_losses.append(running_loss / 2000)
-                running_loss = 0.0
+        # for step, (X_test, y_test) in enumerate(test_loader, 0):
+        #     X_test, y_test = Variable(X_test).cuda(), Variable(y_test).cuda()
+        #     outputs = net(X_test)   
 
-    # test
-    class_correct = [0. for i in range(10)]
-    class_total = [0. for i in range(10)]
-    for data in test_loader:
-        X_test, y_test = data
-        outs = net(Variable(X_test))
-        _, predicted = torch.max(outs.data, 1)
-        acc_sqe = (predicted == y_test).squeeze()
-        for i in range(epochs):
-            label = labels[i]
-            class_correct[label] += acc_sqe[i]
-            class_total[label] += 1
 
-    for i in range(classes):
-        acc = 100 * class_correct[i] / class_total[i]
-        print('Accuracy of {0} : {1:.4f}%'.format(classes[i], acc))
-
-        
-    plt.figure()
-    plt.plot(all_losses)
-    plt.grid(True)
-    plt.show()
 
     print('Finished Training')
 
@@ -89,23 +74,27 @@ class Net(torch.nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = torch.nn.Conv2d(3, 6, 5)
-        self.pool = torch.nn.MaxPool2d(2, 2)
-        self.conv2 = torch.nn.Conv2d(6, 16, 5)
-        self.fc1 = torch.nn.Linear(16*5*5, 120)
-        self.fc2 = torch.nn.Linear(120, 84)
-        self.fc3 = torch.nn.Linear(84, 10)
+        self.conv1 = torch.nn.Conv2d(3, 64, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(64, 128, kernel_size=5)
+        # self.conv3 = torch.nn.Conv2d(32, 64, kernel_size=5)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2)
+        self.dropout = torch.nn.Dropout(p=0.5)
+        self.fc1 = torch.nn.Linear(128*5*5, 512)
+        self.fc2 = torch.nn.Linear(512, 10)
 
     def forward(self, x):
         """
         前向传播
         """
         x = self.pool(fn.relu(self.conv1(x)))
+        x = self.dropout(x)
         x = self.pool(fn.relu(self.conv2(x)))
-        x = x.view(-1, 16*5*5)
+        x = self.dropout(x)
+        # x = self.pool(fn.relu(self.conv3(x)))
+        # x = self.dropout(x)
+        x = x.view(x.size(0), -1)        #  展平多维的卷积图成 (batch_size, 32 * 7 * 7) x.view(-1, 64*5*5)
         x = fn.relu(self.fc1(x))
-        x = fn.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
 
         return x
 
@@ -113,5 +102,5 @@ class Net(torch.nn.Module):
 
 if __name__ == '__main__':
     
-    train(4)
+    train(epochs=64, batch_size=128, log_interval=100)
 
