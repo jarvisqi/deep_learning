1f11ea5e81dd3cdce07340fa80f2650d8b117305
jarvisqi <qdw8911@sina.com>
2018-05-23 13:30:36 +0800

    pytorch 0.4 and model prediction

--- dl_torch/torch_autoencoder.py
+++ dl_torch/torch_autoencoder.py
@@ -3,48 +3,62 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.utils.data as Data
 import torchvision
+import numpy as np
+from skimage import io, transform
 
 
-# class AutoEncoder(nn.Module):
 
-#     def __init__(self):
-#         super(AutoEncoder, self).__init__()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-#         # 压缩
-#         self.encoder = nn.Sequential(
-#             nn.Linear(28*28, 128),
-#             nn.Tanh(),
-#             nn.Linear(128, 64),
-#             nn.Tanh(),
-#             nn.Linear(64, 12),
-#             nn.Tanh(),
-#             nn.Linear(12, 3)                    # 压缩成3个特征, 进行 3D 图像可视化
-#         )
+class AutoEncoder(nn.Module):
+    """自编码
+    
+    Arguments:
+        nn {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
 
-#         # 解压
-#         self.decoder = nn.Sequential(
-#             nn.Linear(3,12),
-#             nn.Tanh(),
-#             nn.Linear(12,64),
-#             nn.Tanh(),
-#             nn.Linear(64,128),
-#             nn.Tanh(),
-#             nn.Linear(128,28*28),
-#             nn.Sigmoid()                        # 激活函数让输出值在 (0, 1)
-#         )
 
-#     def forward(self, x):
-#         encoded = self.encoder(x)
-#         decoded = self.decoder(encoded)
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
 
-#         return encoded, decoded
+        # 压缩
+        self.encoder = nn.Sequential(
+            nn.Linear(28*28, 128),
+            nn.Tanh(),
+            nn.Linear(128, 64),
+            nn.Tanh(),
+            nn.Linear(64, 12),
+            nn.Tanh(),
+            nn.Linear(12, 3)                    # 压缩成3个特征, 进行 3D 图像可视化
+        )
+
+        # 解压
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 12),
+            nn.Tanh(),
+            nn.Linear(12, 64),
+            nn.Tanh(),
+            nn.Linear(64, 128),
+            nn.Tanh(),
+            nn.Linear(128, 28*28),
+            nn.Sigmoid()                        # 激活函数让输出值在 (0, 1)
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+
+        return encoded, decoded
 
 
 def train():
     # 超参数
-    EPOCH = 10
-    BATCH_SIZE = 64
-    LR = 0.005
+    EPOCH = 16
+    BATCH_SIZE = 128
+    LR = 0.001
     N_TEST_IMG = 5          # 到时候显示 5张图片看效果, 如上图一
 
     train_data = torchvision.datasets.MNIST(root='./data/',
@@ -56,16 +70,16 @@ def train():
         dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 
     net = AutoEncoder()
-    net.cuda()
+    net.to(device)
     print(net)
 
     optimizer = torch.optim.Adam(net.parameters(), lr=LR)
-    loss_func = nn.MSELoss()
+    loss_func = nn.MSELoss().to(device)
     for epoch in range(EPOCH):
         for step, (x, y) in enumerate(train_loader):
-            b_x = Variable(x.view(-1, 28*28)).cuda()
-            b_y = Variable(x.view(-1, 28*28)).cuda()
-            b_label = Variable(y).cuda()
+            b_x = x.view(-1, 28*28).to(device)
+            b_y = x.view(-1, 28*28).to(device)
+            b_label = y.to(device)
 
             encoded, decoded = net(b_x)
 
@@ -81,17 +95,27 @@ def train():
     # 需要在序列化和反序列化定义相同的函数名称，但内容可以不一样。否则报错如下：
     # AttributeError: Can't get attribute 'sayhi' on <module '__main__' from
 
-    torch.save(net,"./models/torch/auto_encoder.pt")
+    torch.save(net, "./models/torch/auto_encoder.pt")
+
+
+def read_one_image(path):
+    img = io.imread(path, as_grey=True)
+    img = transform.resize(img, (28, 28), mode='reflect')
+
+    return np.asarray(img)
 
 
 def restore():
     model = torch.load("./models/torch/auto_encoder.pt")
-    print(model)
+    img = read_one_image("./predict_img/numbers/9.jpg")
+    x = np.array([img.reshape(-1)])
+    print(x.shape)
+    x = torch.from_numpy(x).float()
+    x = x.to(device)
+    output = model(x)
+    pred_y = torch.Tensor.argmax(output[0]).data.cpu().numpy()
+    print(pred_y)
 
-
-class AutoEncoder(nn.Module):
-    pass
-    
 
 if __name__ == '__main__':
     # train()
